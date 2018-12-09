e5a047cf0f30582c3b06754fd2849bfeaa7dba87
piatek29535 <piatek29535@student.pwsztar.edu.pl>
2018-12-09 12:33:32 +0100

    ListView + RefreshControll, pobieranie rezultatów z serwera - Done, następnie obczaić pobieranie testów i ukańczanie ich oraz wysyłanie wyników na serwer

--- Screens/ResultScreen.js
+++ Screens/ResultScreen.js
@@ -7,50 +7,71 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, TouchableOpacity} from 'react-native';
+import {Platform, StyleSheet, Text, View, TouchableOpacity, ListView, RefreshControl} from 'react-native';
 
 type Props = {};
-let upActiveFields = ['Nick','Point','Type','Date'];
-let ranking = [['1','2','3','3'] , ['4','5','6','3'] , ['7','8','9','3'] , ['10','11','12','3']]
+const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
 
 export default class ResultScreen extends Component<Props> {
 
-  upActiveFieldsRender(){
-    let row = []
-    let fields = []
-      for(let i = 0;i<4;i++){
-          fields.push(<TouchableOpacity style={styles.activeButton} onPress={() => console.log("")}>
-                        <Text style={styles.activeButtonText}>{upActiveFields[i]}</Text>
-                      </TouchableOpacity>)
-      }
-      row.push(<View style={styles.activeFields}>{fields}</View>)
-    return row
+  constructor() {
+      super();
+      this.componentDidMount;
+      this.state = {
+        refreshing: false,
+        dataSource: ds,
+      };
   }
 
-  rankingFieldsRender(){
-    let rows = [];
-      for(let i = 0;i<4;i++){
-        let row = [];
-          for(let j = 0;j<4;j++){
-              row.push(<Text style={styles.table}>{ranking[i][j]}</Text>)
-          }
-        rows.push(<View style={styles.fields}>{row}</View>)
-      }
+  onRefresh = () => {
+    this.setState({refreshing: true});
+    fetchData().then(() => {
+      this.setState({refreshing: false});
+    });
+  }
 
-    return rows
+
+  componentDidMount(){
+    //https://pwsz-quiz-api.herokuapp.com/api/results
+    fetch('https://pwsz-quiz-api.herokuapp.com/api/results')
+      .then((response) => response.json())
+      .then((responseJson) => {
+          this.setState({
+              dataSource: ds.cloneWithRows(responseJson),
+          })
+      })
+      .catch((error) => {
+          console.error(error)
+      });
   }
 
   render() {
     return (
       <View style={styles.mainContainer}>
+          <View style={styles.headerView}>
+            <Text style={styles.headerText}> Wyniki graczy </Text>
+          </View>
 
-        <View style={styles.headerContainer}>
-          <Text style={styles.textHeader} >Results</Text>
-        </View>
+          <ListView
+          style={styles.listView}
+          dataSource={this.state.dataSource}
+          renderRow={(rowData) =>
+            <View style={styles.results}>
+              <Text style={styles.resultText}>{"Nick: "+rowData.nick}</Text>
+              <Text style={styles.resultText}>{"Wynik: "+rowData.score}</Text>
+              <Text style={styles.resultText}>{"Suma punktów: "+rowData.total}</Text>
+              <Text style={styles.resultText}>{"Typ: "+rowData.type}</Text>
+              <Text style={styles.resultText}>{"Data: "+rowData.date}</Text>
+            </View>
+          }
 
-        {this.upActiveFieldsRender()}
-
-        {this.rankingFieldsRender()}
+            refreshControl={
+              <RefreshControl
+                refreshing={this.state.refreshing}
+                onRefresh={this._onRefresh}
+              />
+            }
+          />
       </View>
     );
   }
@@ -58,72 +79,51 @@ export default class ResultScreen extends Component<Props> {
 
 const styles = StyleSheet.create({
   mainContainer: {
+    backgroundColor:'#00bfff',
+    borderWidth:3,
+    margin:10,
+    marginTop:60,
     flex: 1,
-    padding:20,
-    justifyContent: 'center',
-    backgroundColor: '#E5E5E5',
-  },
-
-// Header Container
-
-  headerContainer:{
     padding:10,
-    marginBottom:15,
-    backgroundColor:'#CCCCCC',
-    borderRadius:5,
-    borderStyle: 'solid',
-    borderWidth: 2,
-    alignItems:'center'
-  },
-  textHeader:{
-    padding:10,
-    fontSize:30,
-    fontWeight:'bold',
-  },
-
-// Active Fields
-  activeFields:{
-    flexDirection:'row',
-    borderStyle:'dashed',
-    borderWidth: 1,
-    borderColor: '#B2B2B2',
     justifyContent: 'center',
   },
-  activeButton:{
-    flex:1,
-    width:80,
-    height:80,
-    backgroundColor: '#7F7F7F',
-    borderStyle:'dashed',
-    borderWidth: 1,
-    borderColor: '#000000',
+//Header
+  headerView:{
     justifyContent:'center',
-    alignItems:'center'
+    alignItems:'center',
+    borderBottomWidth:4,
+    borderRadius:20,
+    borderColor:'#FFFFFF',
   },
-  activeButtonText:{
-    textAlign:'center',
-    fontWeight:'bold',
-    fontSize:25,
+  headerText:{
+    marginBottom:10,
+    fontSize:30,
+    fontFamily:"RobotoSlab-Bold",
+    color:'#e0ebeb',
+    textShadowColor: 'rgba(0, 0, 0, 0.75)',
+    textShadowOffset: {width: 3, height: 3},
   },
 
-
-//Normal Fields
-  fields:{
-    flexDirection:'row',
-    borderStyle:'dashed',
-    borderWidth: 1,
-    borderColor: '#000000'
+//Wyniki
+  listView:{
+    marginTop:20,
   },
-  table:{
-    flex:1,
-    padding:22,
-    width:80,
-    height:80,
-    backgroundColor: '#FFFFFF',
+
+  results:{
+    borderWidth:2,
     borderStyle:'dashed',
-    borderWidth: 1,
-    borderColor: '#000000'
+    borderColor:'#FFFFFF',
+    borderRadius:5,
+    backgroundColor:"#00ace6",
+    margin:5,
+    padding:15,
+  },
+
+  resultText:{
+    fontSize:15,
+    fontFamily:"Oswald-Bold",
+    letterSpacing:1
+
   }
-
 
 });
